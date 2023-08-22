d5185ef9b7dfdcd5d59f4a3d75a2b7d816d6f494
Jake <zamorejake@gmail.com>
2023-08-22 04:04:43 -0500

    setup posts and styling with them. adding comments to the them works too

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { TechBlogPost, User } = require('../models');
+const { TechBlogPost, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
@@ -34,6 +34,16 @@ router.get('/post/:id', async (req, res) => {
           model: User,
           attributes: ['name'],
         },
+        {
+          model: Comment, 
+          attributes: ['comment', 'date_posted'],
+          include: [
+            {
+              model: User,
+              attributes: ['name'],
+            },
+          ],
+        },
       ],
     });
 
@@ -41,8 +51,25 @@ router.get('/post/:id', async (req, res) => {
 
     res.render('profileid', {
       post,
+      comments: post.comments, 
       logged_in: req.session.logged_in,
     });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/add-comment/:id', async (req, res) => {
+  try {
+    const newComment = {
+      comment: req.body.comment,
+      user_id: req.session.user_id, 
+      post_id: req.body.post_id, 
+    };
+
+    await Comment.create(newComment);
+
+    res.redirect(`/post/${req.params.id}`);
   } catch (err) {
     res.status(500).json(err);
   }
